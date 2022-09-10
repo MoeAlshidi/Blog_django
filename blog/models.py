7d7c9f538ed042d9979fda77cb01624f16d0f3cc
Moe Alshidi <mohammedalshidi55@gmail.com>
2022-09-11 00:07:13 +0400

    views, templete

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,32 @@
 
 from django.db import models
+from django.contrib.auth.models import User 
 from django.utils import timezone
 
-# Create your models here.
+
+
+class PublishedManager(models.Manager):
+  def get_queryset(self):
+    return super().get_queryset()\
+                  .filter(status=Post.Status.PUBLISHED)
 
 class Post(models.Model):
+
+  class Status(models.TextChoices):
+    DRAFT= 'DF','Draft'
+    PUBLISHED='PB','Published'
+
   title=models.CharField(max_length=250)
   slug=models.SlugField(max_length=250)
   body=models.TextField()
   publish=models.DateTimeField(default=timezone.now)
   created=models.DateTimeField(auto_now_add=True)
   updated=models.DateTimeField(auto_now_add=True)
+  status=models.CharField(max_length=2,choices=Status.choices,default=Status.DRAFT)
+  author=models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_posts')
+  objects=models.Manager() #The default manager
+  published=PublishedManager() #Our custom manager
+
 
   
   class Meta:
